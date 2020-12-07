04b00dbaa64a1499756eafacab775e1af5566bca
Mologorsky Alan <a.mologorsky@corp.mail.ru>
2020-12-08 00:31:59 +0300

    Add callbackapi_handler.
    [add] confirmation request support
    [add] "new message" request

--- application.py
+++ application.py
@@ -20,10 +20,27 @@ async def hello(request):
     return web.Response(text="Hello, world")
 
 
+async def callbackapi_handler(request):
+    """Callback API request handler"""
+    data = await request.json()
+    # here data['group_id'] can be checked
+    if data['type'] == 'confirmation':
+        # TODO: remove hard code
+        return web.Response(text='ee49c27d')
+    # new message
+    elif data['type'] == 'message_new':
+        logging.info(data['object'])
+
+    return web.Response(text='OK')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
     app = web.Application()
     logging.basicConfig(level=logging.DEBUG)
     logging.debug('starting!')
-    app.add_routes([web.get('/', hello)])
+    app.add_routes([
+        web.get('/', hello),
+        web.post('/callback/send_notification', callbackapi_handler)
+        ])
     web.run_app(app, port=port)
