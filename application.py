bec187c75fe50a729ad42a096993ab8971bb3365
Mologorsky Alan <a.mologorsky@corp.mail.ru>
2020-12-07 23:22:05 +0300

    Logging + middleware.
    [add] test middleware
    [add] logging
    [upd] README

--- application.py
+++ application.py
@@ -1,15 +1,29 @@
+import logging
 import os
 
 from aiohttp import web
+from aiohttp.web import middleware
+
+
+@middleware
+async def secret_validate(request, handler):
+
+    resp = await handler(request)
+    resp.text = resp.text + ' wink'
+    return resp
 
 
 async def hello(request):
     """Test request handler."""
+    logging.info('hello world incoming request!')
+    logging.debug('DEBUG on hello world incoming request!')
     return web.Response(text="Hello, world")
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
     app = web.Application()
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug('starting!')
     app.add_routes([web.get('/', hello)])
     web.run_app(app, port=port)
